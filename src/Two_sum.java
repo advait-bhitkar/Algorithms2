eda5198caac54fd541bbcd40725ebac312464af8
Advait Bhitkar <advait@advaitbhitkar.com>
2023-06-15 13:55:36 +0530

    Two Sum

--- src/Two_sum.java
+++ src/Two_sum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Two_sum {
 
     /*
@@ -8,5 +10,20 @@ public class Two_sum {
     */
     public static void findPair(int[] nums, int target){
 
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                System.out.printf("Pair found (%d, %d)", nums[map.get(target - nums[i])], nums[i]);
+            }else {
+                map.put(nums[i], i);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[] nums = { 8, 7, 2, 5, 3, 5, 1 };
+        int target = 10;
+        findPair(nums, target);
     }
 }
