38edf9b65b5c5abc4b00b779d1238f675a1aa4fd
Advait Bhitkar <advait@advaitbhitkar.com>
2023-06-15 13:28:57 +0530

    Created Algorithms project

---
+++ src/Two_sum.java
@@ -0,0 +1,12 @@
+public class Two_sum {
+
+    /*
+    * Use 2 loops for brute force
+    * Sorting the Array
+    * Using HashMap
+    * If there are duplicates in the array use list
+    */
+    public static void findPair(int[] nums, int target){
+
+    }
+}
